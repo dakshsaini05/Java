d8deb4c153d87e6c19ba90702d225d6b51c5d9eb
dakshsaini05 <dakshsince2005@gmail.com>
2024-09-11 17:13:45 +0530

    basic of java

--- Main.java
+++ Main.java
@@ -1,6 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import javax.xml.transform.Source;
 import java.util.Scanner;
 
 public class Main {
