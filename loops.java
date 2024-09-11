7f81aec4accefc80633f3316a00bdfe0156b1e32
dakshsaini05 <dakshsince2005@gmail.com>
2024-09-11 22:48:12 +0530

    while loop - prime factor question

--- loops.java
+++ loops.java
@@ -49,14 +49,38 @@ public class loops {
 //        }
 
         //Count digits of a number
-        int count = 0;
-        System.out.print("Enter number(don't put zero in between): ");
-        int num1 = sc.nextInt();
+//        int count = 0;
+//        System.out.print("Enter number(don't put zero in between): ");
+//        int num1 = sc.nextInt();
+//
+//        while(num1 > 0) {
+//            num1 /= 10;
+//            count++;
+//        }
+//        System.out.println(count);
 
-        while(num1 > 0) {
-            num1 /= 10;
-            count++;
+        // Reverse a number and print it vertically
+//        System.out.print("Enter number: ");
+//        int r = sc.nextInt();
+//        int rem = 0, sum = 0;
+//        while(r > 0) {
+//            rem = r % 10;
+//            System.out.println(rem);
+//            r /= 10;
+//        }
+
+        // Prime factor
+        System.out.print("Enter number to find prime factor: ");
+        int prime_fact = sc.nextInt();
+        int i = 2;
+        while(prime_fact > 1) {
+            if(prime_fact%i == 0) {
+                prime_fact /= i;
+                System.out.println(i); //print the factor
+            }
+            else {
+                i++; //increment when i can no further divisible from prime_fact
+            }
         }
-        System.out.println(count);
     }
 }
