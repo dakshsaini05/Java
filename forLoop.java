98c5ab95dbd1b4c778992d47ffe6a717aa82e5c0
dakshsaini05 <dakshsince2005@gmail.com>
2024-09-24 15:59:47 +0530

    Pattern printing - numbered right-angled triangle

--- forLoop.java
+++ forLoop.java
@@ -5,6 +5,7 @@ public class forLoop {
         Scanner sc = new Scanner(System.in);
 
         //Rectangle
+        System.out.print("Enter number: ");
         int n = sc.nextInt();
         for(int i=1;i<=n;i++) {
             for(int j=1;j<=n;j++) {
@@ -13,6 +14,41 @@ public class forLoop {
             System.out.println();
         }
 
+        //Right angled triangle
+        System.out.print("Enter number: ");
+        int b = sc.nextInt();
+        for(int i=1;i<=b;i++) {
+            for(int j=1;j<=i;j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Number right-angled triangle 1
+        for(int i=1;i<=5;i++) {
+            for(int j=1;j<=i;j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        // Number right-angled triangle 2
+        for(int i=1;i<=5;i++) {
+            for(int j=1;j<=i;j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+        // Number right-angled triangle 3
+        int count = 1;
+        for(int i=1;i<=5;i++) {
+            for(int j=1;j<=i;j++) {
+                System.out.print(count + " ");
+                count++;
+            }
+            System.out.println();
+        }
 
     }
 }
