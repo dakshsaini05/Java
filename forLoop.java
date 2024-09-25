a03142292bd4db50a0fdd63d8ca3c47d5059158d
dakshsaini05 <dakshsince2005@gmail.com>
2024-09-25 16:42:22 +0530

    Pattern Printing

--- forLoop.java
+++ forLoop.java
@@ -5,49 +5,83 @@ public class forLoop {
         Scanner sc = new Scanner(System.in);
 
         //Rectangle
-        System.out.print("Enter number: ");
-        int n = sc.nextInt();
-        for(int i=1;i<=n;i++) {
-            for(int j=1;j<=n;j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
+//        System.out.print("Enter number: ");
+//        int n = sc.nextInt();
+//        for(int i=1;i<=n;i++) {
+//            for(int j=1;j<=n;j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//
+//        //Right angled triangle
+//        System.out.print("Enter number: ");
+//        int b = sc.nextInt();
+//        for(int i=1;i<=b;i++) {
+//            for(int j=1;j<=i;j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//
+//        // Number right-angled triangle 1
+//        for(int i=1;i<=5;i++) {
+//            for(int j=1;j<=i;j++) {
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+//
+//        // Number right-angled triangle 2
+//        for(int i=1;i<=5;i++) {
+//            for(int j=1;j<=i;j++) {
+//                System.out.print(i);
+//            }
+//            System.out.println();
+//        }
+//
+//        // Number right-angled triangle 3
+//        int count = 1;
+//        for(int i=1;i<=5;i++) {
+//            for(int j=1;j<=i;j++) {
+//                System.out.print(count + " ");
+//                count++;
+//            }
+//            System.out.println();
+//        }
 
-        //Right angled triangle
-        System.out.print("Enter number: ");
-        int b = sc.nextInt();
-        for(int i=1;i<=b;i++) {
-            for(int j=1;j<=i;j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-
-        // Number right-angled triangle 1
         for(int i=1;i<=5;i++) {
-            for(int j=1;j<=i;j++) {
+            for(int j=1;j<=5;j++) {
                 System.out.print(j);
             }
             System.out.println();
         }
 
-        // Number right-angled triangle 2
         for(int i=1;i<=5;i++) {
-            for(int j=1;j<=i;j++) {
-                System.out.print(i);
+            for(int j=5;j>=i;j--) {
+                System.out.print("* ");
             }
-            System.out.println();
+            System.out.println(" ");
         }
 
-        // Number right-angled triangle 3
-        int count = 1;
         for(int i=1;i<=5;i++) {
-            for(int j=1;j<=i;j++) {
-                System.out.print(count + " ");
-                count++;
+            for(int s=5;s>=i;s--) {
+                System.out.print(" ");
             }
-            System.out.println();
+            for(int j=1;j<=i;j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+        for(int i=1;i<=5;i++) {
+            for(int s=1;s<=i;s++) {
+                System.out.print(" ");
+            }
+            for(int j=5;j>=i;j--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
         }
 
     }
