7bc421ae0ef2460ec120a2adff2f2c4801c0c327
dakshsaini05 <dakshsince2005@gmail.com>
2024-09-27 00:04:58 +0530

    Pattern Printing

--- forLoop.java
+++ forLoop.java
@@ -4,85 +4,87 @@ public class forLoop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        //Rectangle
-//        System.out.print("Enter number: ");
-//        int n = sc.nextInt();
-//        for(int i=1;i<=n;i++) {
-//            for(int j=1;j<=n;j++) {
-//                System.out.print("* ");
-//            }
-//            System.out.println();
-//        }
-//
-//        //Right angled triangle
-//        System.out.print("Enter number: ");
-//        int b = sc.nextInt();
-//        for(int i=1;i<=b;i++) {
-//            for(int j=1;j<=i;j++) {
-//                System.out.print("* ");
-//            }
-//            System.out.println();
-//        }
-//
-//        // Number right-angled triangle 1
-//        for(int i=1;i<=5;i++) {
-//            for(int j=1;j<=i;j++) {
-//                System.out.print(j);
-//            }
-//            System.out.println();
-//        }
-//
-//        // Number right-angled triangle 2
-//        for(int i=1;i<=5;i++) {
-//            for(int j=1;j<=i;j++) {
-//                System.out.print(i);
-//            }
-//            System.out.println();
-//        }
-//
-//        // Number right-angled triangle 3
-//        int count = 1;
-//        for(int i=1;i<=5;i++) {
-//            for(int j=1;j<=i;j++) {
-//                System.out.print(count + " ");
-//                count++;
-//            }
-//            System.out.println();
-//        }
-
-        for(int i=1;i<=5;i++) {
-            for(int j=1;j<=5;j++) {
-                System.out.print(j);
+        //Right-angled triangle
+        for(int i=1;i<=4;i++) {
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
             }
             System.out.println();
         }
-
-        for(int i=1;i<=5;i++) {
-            for(int j=5;j>=i;j--) {
+        //Inverse right-angled triangle
+        for(int i=1;i<=4;i++) {
+            for(int j=1;j<=(4-i)+1;j++) {
                 System.out.print("* ");
             }
             System.out.println(" ");
         }
 
+        //Opposite sided right-angled triangle
         for(int i=1;i<=5;i++) {
-            for(int s=5;s>=i;s--) {
-                System.out.print(" ");
+            for(int j=1;j<=5;j++) {
+                if(j <= (5 - i)) {
+                    System.out.print(" ");
+                }
+                else {
+                    System.out.print("*");
+                }
             }
+            System.out.println();
+        }
+
+        //Opposite sided inverse right-angled triangle
+//        int a = 0;
+        for(int i=1;i<=5;i++) {
+            for(int j=1;j<=5;j++) {
+                if(j > i-1) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+//            a++;
+            System.out.println();
+        }
+
+        //Odd number triangle
+        for(int i=1;i<=5;i++) {
+            int k =0;
             for(int j=1;j<=i;j++) {
-                System.out.print("*");
+                System.out.print((k * 2) + 1);
+                k++;
             }
-            System.out.println(" ");
+            System.out.println();
         }
 
+        //Odd number sequence triangle
+        int nextNum = 1;
         for(int i=1;i<=5;i++) {
-            for(int s=1;s<=i;s++) {
-                System.out.print(" ");
+            for(int j=1;j<=i;j++) {
+                System.out.print(nextNum + " ");
+                nextNum += 2;
             }
-            for(int j=5;j>=i;j--) {
-                System.out.print("*");
-            }
-            System.out.println(" ");
+            System.out.println();
         }
 
+        //Even number triangle
+        for(int i=1;i<=5;i++) {
+            int k = 1;
+            for(int j=1;j<=i;j++) {
+                System.out.print(k*2 + " ");
+                k++;
+            }
+            System.out.println();
+        }
+
+        //Even number sequence triangle
+        int nextNum2 = 2;
+        for(int i=1;i<=5;i++) {
+            for(int j=1;j<=i;j++) {
+                System.out.print(nextNum2 + " ");
+                nextNum2 += 2;
+            }
+            System.out.println();
+        }
     }
 }
