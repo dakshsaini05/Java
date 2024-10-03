cc27f5c3a3d7916067ee6f07e612e83d84b4efb9
dakshsaini05 <dakshsince2005@gmail.com>
2024-10-03 17:05:34 +0530

    Pattern Printing

--- forLoop.java
+++ forLoop.java
@@ -86,5 +86,146 @@ public class forLoop {
             }
             System.out.println();
         }
+
+        //Hollow rectangle
+        for(int i=1;i<=5;i++) {
+            for(int j=1;j<=5;j++) {
+                if(i == 1 || i == 5 || j == 1 || j == 5) {
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        //Pyramid triangle
+        for(int i=1;i<=5;i++) {
+            for(int j=1;j<=5;j++) {
+                if(j <= 5 - i) {
+                    System.out.print(" ");
+                }
+                else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+
+        //Hollow triangle
+        int n = 5;
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=n;j++) {
+                if(j == n-i+1 || i == 1 || j == 1) {
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        //Hollow 180 degree K
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=n;j++) {
+                if(j == n-i+1 || i == 1 || j == 1) {
+                    System.out.print(j + " ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+
+            for(int j=n-1;j>0;j--) {
+                if(j == n-i+1 || i == 1 || j == 1) {
+                    System.out.print(j + " ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        //Number pyramid
+        for(int i=1;i<=5;i++) {
+            for(int j=1;j<=n;j++) {
+                if(j<=(n-i)+1) System.out.print("  ");
+                else System.out.print(i + " ");
+            }
+
+            for(int j=1;j<=n;j++) {
+                if(j > i) System.out.print("  ");
+                else System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+
+        //Number increasing triangle
+        for(int i=1;i<=5;i++) {
+            for(int j=1;j<=i;j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        //Number increasing reverse triangle
+        for(int i=1;i<=5;i++) {
+            for(int j=1;j<=(5-i)+1;j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        //Number changing triangle
+        int count = 1;
+        for(int i=1;i<=5;i++) {
+            for(int j=1;j<=i;j++) {
+                System.out.print(count + " ");
+                count++;
+            }
+            System.out.println();
+        }
+
+        //Zero-One triangle
+        for(int i=1;i<=5;i++) {
+            for(int j=1;j<=i;j++) {
+                if((i+j) % 2 == 0) System.out.print("1 ");
+                else System.out.print("0 ");
+            }
+            System.out.println();
+        }
+
+        //Palindrome pyramid*
+        for(int i=1;i<=5;i++) {
+            for(int j=5;j>=2;j--) {
+                if(j <= (n-i)+1) System.out.print("  ");
+                else System.out.print(j + " ");
+            }
+
+            for(int j=1;j<=5;j++) {
+                if(j > i) System.out.print("  ");
+                else System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        // Rhombus pattern
+        for(int i=1;i<=5;i++) {
+            for(int j=5;j>=1;j--) {
+                if(j >= (n-i)+1) System.out.print("  ");
+                else System.out.print("* ");
+            }
+
+            for(int j=1;j<=5;j++) {
+                if(j > i) System.out.print("  ");
+                else System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
     }
 }
