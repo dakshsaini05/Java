a621708ea5a12c0e3ee6972035642d1f4a5331e9
dakshsaini05 <dakshsince2005@gmail.com>
2025-03-04 13:44:45 +0530

    Nested try catch

--- DSA_450/EH/main.java
+++ DSA_450/EH/main.java
@@ -1,9 +1,14 @@
 package DSA_450.EH;
 
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
 
 //        Arithumetic exception
 //        System.out.println("Hello");
@@ -36,17 +41,45 @@ public class main {
 //        System.out.println("Number is " + num);
 
 //        Try with multiple catch
+//        int[] arr = new int[2];
+//        arr[0] = 10;
+//        arr[1] = 20;
+//        try {
+//            System.out.println(arr[2]/0);
+//        } catch (ArithmeticException e1) {
+//            System.out.println(e1);
+//        } catch (ArrayIndexOutOfBoundsException e2) {
+//            System.out.println(e2);
+//        } catch (Exception e3) {
+//            System.out.println(e3);
+//        }
+
+//        Nested try catch
         int[] arr = new int[2];
-        arr[0] = 10;
-        arr[1] = 20;
         try {
-            System.out.println(arr[2]/0);
-        } catch (ArithmeticException e1) {
-            System.out.println(e1);
-        } catch (ArrayIndexOutOfBoundsException e2) {
-            System.out.println(e2);
-        } catch (Exception e3) {
-            System.out.println(e3);
+            System.out.print("Enter the value for index 0: ");
+            arr[0] = sc.nextInt();
+            System.out.print("Enter the value for index 1: ");
+            arr[1] = sc.nextInt();
+
+            try {
+                System.out.print("Enter the index value for 0th index: ");
+                int nindex = sc.nextInt();
+                int numerator = arr[nindex];
+                System.out.print("Enter the index value for 1st index: ");
+                int dindex = sc.nextInt();
+                int denominator = arr[dindex];
+                double result = (double) numerator / denominator;
+                System.out.println(result);
+            }
+            catch (ArithmeticException e1) {
+                System.out.println("can't"+e1.getMessage());
+            }
+            catch (ArrayIndexOutOfBoundsException e2) {
+                System.out.println("Please enter correct index");
+            }
+        } catch (InputMismatchException e1) {
+            System.out.println("Please enter integer value");
         }
     }
 }
